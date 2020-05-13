f6ce9d075712b21733224b0bd8aed914a8fac8a5
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-05-13 11:23:33 +0700

    add modal component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import "./script/component/header.js";
+import "./script/component/modal.js";
 import "./style/style.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.min.css";
